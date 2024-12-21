bb7d8a847ebfee92e4c48fe2d1935293c2ac5f29
Sonupalsingh <73596998+Sonupalsingh@users.noreply.github.com>
2024-12-21 12:52:24 +0530

    Create adminer.sh

---
+++ adminer.sh
@@ -0,0 +1,38 @@
+##########adminer for sql##########
+##Step 1: Update System
+sudo yum update -y
+
+
+
+##Step 2: Install Apache Web Server
+sudo yum install httpd -y
+sudo systemctl start httpd
+sudo systemctl enable httpd
+
+
+
+##Step 3: Install PHP
+sudo yum install php php-mysqlnd -y
+sudo systemctl restart httpd
+
+
+##Step 4: Download Adminer
+sudo mkdir -p /var/www/html/adminer
+sudo curl -o /var/www/html/adminer/index.php https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1.php
+
+
+##Step 5: Adjust Permissions
+sudo chown -R apache:apache /var/www/html/adminer
+sudo chmod -R 755 /var/www/html/adminer
+
+
+###Step 6: Configure Firewall
+sudo firewall-cmd --permanent --add-service=http
+sudo firewall-cmd --reload
+
+
+
+
+
+
+##############################################################
